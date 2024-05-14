2ca2457b252b208f3c2ca6e95889e5000cf62f20
chayanon12 <chayanonolo@gmail.com>
2024-05-14 09:47:09 +0700

    How to use api

--- Service.cjs
+++ Service.cjs
@@ -3,7 +3,7 @@ const app = express();
 const port = 3001;
 const oracledb = require("oracledb");
 const LoginService = require("./Conncetion/LoginService.cjs");
-
+const serialmaster = require("./WorkService/serialMaster.cjs");
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -15,6 +15,7 @@ app.use(express.json());
 
 
 app.post("/login", LoginService.login);
+app.post("/CheckrunCode", serialmaster.runningCode);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
