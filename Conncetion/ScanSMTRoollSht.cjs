338234c96e372be1c0cd63915e2eed28390f97e6
Mayxsasii <Lovemay19123@gmail.com>
2024-05-14 14:44:17 +0700

    may add

---
+++ Conncetion/ScanSMTRoollSht.cjs
@@ -0,0 +1,45 @@
+const {
+    ConnectPG_DB,
+    DisconnectPG_DB,
+    ConnectOracleDB,
+    DisconnectOracleDB,
+  } = require("./DBConn.cjs");
+
+  module.exports.getLot = async function (req, res) {
+    try {
+      const { txt_lotno } = req.body;
+      console.log('txt_lotno:', txt_lotno);
+  
+      const connect = await ConnectOracleDB("FPC");
+      let query = "";
+      query += `SELECT NVL(L.LOT_PRD_NAME, ' ') AS PRD_NAME, `;
+      query += `NVL(L.LOT_ROLL_NO, ' ') AS ROLL_NO, `;
+      query += `TO_CHAR(LISTAGG(L.LOT || `;
+      query += `DECODE(T1.LTR_FROM_LOT, NULL, '', ',' || T1.LTR_FROM_LOT) || `;
+      query += `DECODE(T2.LTR_FROM_LOT, NULL, '', ',' || T2.LTR_FROM_LOT) || `;
+      query += `DECODE(T3.LTR_FROM_LOT, NULL, '', ',' || T3.LTR_FROM_LOT) || `;
+      query += `DECODE(T4.LTR_FROM_LOT, NULL, '', ',' || T4.LTR_FROM_LOT) || `;
+      query += `DECODE(T5.LTR_FROM_LOT, NULL, '', ',' || T5.LTR_FROM_LOT), ',') `;
+      query += `WITHIN GROUP (ORDER BY L.LOT ASC)) AS LOT_ALL `;
+      query += `FROM FPC.FPC_LOT L `;
+      query += `LEFT JOIN FPC_LOT_TRANSFER T1 ON L.LOT = T1.LTR_LOT `;
+      query += `LEFT JOIN FPC_LOT_TRANSFER T2 ON T1.LTR_FROM_LOT = T2.LTR_LOT `;
+      query += `LEFT JOIN FPC_LOT_TRANSFER T3 ON T2.LTR_FROM_LOT = T3.LTR_LOT `;
+      query += `LEFT JOIN FPC_LOT_TRANSFER T4 ON T3.LTR_FROM_LOT = T4.LTR_LOT `;
+      query += `LEFT JOIN FPC_LOT_TRANSFER T5 ON T4.LTR_FROM_LOT = T5.LTR_LOT `;
+      query += `WHERE L.LOT = '${txt_lotno}' `;
+      query += `GROUP BY NVL(L.LOT_PRD_NAME, ' '), `;
+      query += `NVL(L.LOT_ROLL_NO, ' '), `;
+      query += `DECODE(L.LOT_PRIORITY, 'FINAL_GATE_LOT_PRIORITY_SKIP', 'Y', 'N')`;
+  
+      console.log('query:', query);
+  
+      const result = await connect.execute(query);
+      DisconnectOracleDB(connect);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("ข้อผิดพลาดในการค้นหาข้อมูล:", error.message);
+      res.status(500).send("ข้อผิดพลาดในการค้นหาข้อมูล");
+    }
+  };
+  
--- Service.cjs
+++ Service.cjs
@@ -6,6 +6,8 @@ const LoginService = require("./Conncetion/LoginService.cjs");
 const serialmaster = require("./WorkService/serialMaster.cjs");
 const MenuService = require("./Conncetion/munuService.cjs");
 
+const ScanSMTRollSht = require("./Conncetion/ScanSMTRoollSht.cjs");
+
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -20,6 +22,8 @@ app.post("/api/login", LoginService.login);
 app.post("/api/MenuName", MenuService.Menuname);
 
 app.post("/api/CheckrunCode", serialmaster.SerialCodeName);
+
+app.post("/api/getLot", ScanSMTRollSht.getLot);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
