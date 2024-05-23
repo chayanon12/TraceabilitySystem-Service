df8d6bbc4f1453ea3f14c2a5e483fe6fbcaa75a8
Mayxsasii <Lovemay19123@gmail.com>
2024-05-23 11:39:40 +0700

    add api ScanSMTRollSht

--- Service.cjs
+++ Service.cjs
@@ -4,8 +4,9 @@ const port = 3001;
 const oracledb = require("oracledb");
 const LoginService = require("./Conncetion/LoginService.cjs");
 const serialmaster = require("./WorkService/serialMaster.cjs");
-const MenuService = require("./Conncetion/munuService.cjs");
+const MenuService = require("./Conncetion/menuService.cjs");
 const ScanSMTRollSht = require("./Conncetion/ScanSMTRoollSht.cjs");
+
 
 
 app.use(express.json());
@@ -23,6 +24,10 @@ app.post("/api/MenuName", MenuService.Menuname);
 app.post("/api/CheckrunCode", serialmaster.SerialCodeName);
 
 app.post("/api/getLot", ScanSMTRollSht.getLot);
+app.post("/api/getProduct", ScanSMTRollSht.getProduct);
+app.post("/api/GetSerialProductByProduct", ScanSMTRollSht.GetSerialProductByProduct);
+app.post("/api/GetRollLeafTotalByLot", ScanSMTRollSht.GetRollLeafTotalByLot);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
