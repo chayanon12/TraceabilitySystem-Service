b8a351c1480458536ceaad7cfb55455a032010cf
Mayxsasii <Lovemay19123@gmail.com>
2024-06-06 11:53:30 +0700

    ..

--- Service.cjs
+++ Service.cjs
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3001;
 const oracledb = require("oracledb");
+
 // Import routes
 const commonRoutes = require("./routes/Common.cjs");
 const LoginService = require("./routes/loginRoutes.cjs");
