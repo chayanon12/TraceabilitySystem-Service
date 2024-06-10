031660df0c1ca86bd7275bea039bceadf325e506
Mayxsasii <Lovemay19123@gmail.com>
2024-06-10 10:50:45 +0700

    may

--- WorkService/Model_Common.cjs
+++ WorkService/Model_Common.cjs
@@ -5,6 +5,8 @@ const {
   DisconnectOracleDB,
 } = require("../Conncetion/DBConn.cjs");
 const oracledb = require("oracledb");
+const { queueRequests } = require("oracledb");
+const { writeLogError } = require("../Common/LogFuction.cjs");
 
 ///------Example
 module.exports.xxxxxx = async function (req, res) {
@@ -96,15 +98,34 @@ module.exports.getfactory = async function (req, res) {
   }
 };
 
+// module.exports.getlotserialcountdata = async function (req, res) {
+//   try {
+//     const { Lotno } = req.body;
+//     var query = "";
+//     const client = await ConnectPG_DB();
+//     query = `SELECT * from "Traceability".trc_000_common_getlotserialcountdata('${Lotno}')`;
+//     const result = await client.query(query);
+//     await DisconnectPG_DB(client);
+//     res.status(200).json(result.rows);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
 module.exports.getlotserialcountdata = async function (req, res) {
+  var query = "";
   try {
-    var query = "";
     const client = await ConnectPG_DB();
-    query = ``;
+    const { Lotno } = req.body;
+    query += `SELECT * from "Traceability".trc_000_common_getlotserialcountdata('${Lotno}')`;
     const result = await client.query(query);
-    await DisconnectPG_DB(client);
-    res.status(200).json({ Result: result });
+    if (result.rows.length > 0) {
+      res.status(200).json(result.rows);
+      DisconnectPG_DB(client);
+    }
   } catch (error) {
+    writeLogError(error.message, query);
+    console.log(error);
     res.status(500).json({ message: error.message });
   }
 };
--- routes/Common.cjs
+++ routes/Common.cjs
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Common = require("../WorkService/Model_Common.cjs");
 
-router.get("/GetProductData", Common.GetProductData);
+ router.get("/GetProductData", Common.GetProductData);
  router.post("/getProductDataByLot", Common.GetProductNameByLot);
-
+ router.post("/getlotserialcountdata", Common.getlotserialcountdata);
+ 
 module.exports = router;
