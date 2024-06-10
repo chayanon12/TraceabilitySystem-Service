4b602d3cfd18e37ddb8e2526db2ad299a3eaff40
chayanon12 <chayanonolo@gmail.com>
2024-06-10 08:50:43 +0700

    Add Function

--- routes/ScanSMTRoollShtRoutes.cjs
+++ routes/ScanSMTRoollShtRoutes.cjs
@@ -4,7 +4,7 @@ const ScanSMTRollSht = require("../WorkService/Model_ScanSMTRoollSht.cjs");
 
 router.post("/getLot", ScanSMTRollSht.getLot);
 router.post("/getProduct", ScanSMTRollSht.getProduct);
-router.post("/GetSerialProductByProduct", ScanSMTRollSht.GetSerialProductByProduct);
+// router.post("/GetSerialProductByProduct", ScanSMTRollSht.GetSerialProductByProduct);
 router.post("/GetRollLeafTotalByLot", ScanSMTRollSht.GetRollLeafTotalByLot);
 
 
