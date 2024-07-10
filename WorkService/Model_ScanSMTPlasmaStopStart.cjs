2055c19f354b7efa2d5c1bf47a48f101d4854d2f
milk953 <milk.the@hotmail.com>
2024-07-10 11:54:06 +0700

    update

--- WorkService/Model_ScanSMTPlasmaStopStart.cjs
+++ WorkService/Model_ScanSMTPlasmaStopStart.cjs
@@ -15,6 +15,36 @@ module.exports.getStartStopRecordTimeByPackingNo = async function (req, res) {
         const client = await ConnectPG_DB();
         const result = await client.query(query);
         await DisconnectPG_DB(client);
+        res.status(200).json(result.rows[0]);
+    } catch (err) {
+        writeLogError(err.message, query);
+        res.status(500).json({ message: err.message });
+    }
+};
+
+module.exports.getStartStopRecordTimeByPartialNo = async function (req, res) {
+    try {
+        const p_data = JSON.stringify(req.body);
+        let query = "";
+        query = ` select * from "Traceability".trc_011_stopstartrecordtime_getstartstoprecordtimebypartialno('${p_data}'); `;
+        const client = await ConnectPG_DB();
+        const result = await client.query(query);
+        await DisconnectPG_DB(client);
+        res.status(200).json(result.rows[0]);
+    } catch (err) {
+        writeLogError(err.message, query);
+        res.status(500).json({ message: err.message });
+    }
+};
+
+module.exports.setStartStopRecordTimeByPartialNo = async function (req, res) {
+    try {
+        const p_data = JSON.stringify(req.body);
+        let query = "";
+        query = ` CALL "Traceability".trc_011_stopstartrecordtime_setstartstoprecordtimebypartialno('${p_data}'); `;
+        const client = await ConnectPG_DB();
+        const result = await client.query(query);
+        await DisconnectPG_DB(client);
         res.status(200).json(result.rows);
     } catch (err) {
         writeLogError(err.message, query);
