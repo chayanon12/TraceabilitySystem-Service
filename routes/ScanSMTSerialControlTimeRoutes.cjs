1549fef21b0a5d95b5efa9221214277d45884bca
milk953 <milk.the@hotmail.com>
2024-07-22 15:22:47 +0700

    update

--- routes/ScanSMTSerialControlTimeRoutes.cjs
+++ routes/ScanSMTSerialControlTimeRoutes.cjs
@@ -3,5 +3,6 @@ const router = express.Router();
 const ScanSMTSerialControl = require("../WorkService/Model_ScanSMTSerialControlTime.cjs");
 
 router.post("/setSerialProcControlTimeTable", ScanSMTSerialControl.SetSerialProcControlTimeTable);
+router.post("/getserialproccontroltimetable", ScanSMTSerialControl.GetSerialProcControlTimeTable);
 
 module.exports = router;
