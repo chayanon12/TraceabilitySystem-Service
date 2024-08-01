466fd893e6a0d4d5703d307e345844929a94db66
chayanon12 <chayanonolo@gmail.com>
2024-08-01 14:39:25 +0700

    add new api

--- Service.cjs
+++ Service.cjs
@@ -27,6 +27,7 @@ const ScanSMTSerialControl = require("./routes/ScanSMTSerialControlTimeRoutes.cj
 const SpotHeat = require("./routes/ScanSMTSerialSpotHeat.cjs");
 const ScanSMTSerialSht = require("./routes/ScanSMTSerialShtConfirmRoutes.cjs");
 const ELT_Type = require("./routes/ELTType.cjs");
+const AVIconfirm = require("./routes/AVIConfirm.cjs");
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -62,6 +63,7 @@ app.use("/api",ScanSMTSerialControl);
 app.use("/api", SpotHeat);
 app.use("/api", ScanSMTSerialSht);
 app.use("/api",ELT_Type)
+app.use("/api",AVIconfirm)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
