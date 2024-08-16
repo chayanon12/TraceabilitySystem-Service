567a6e06ba5fe61e4e7d7eff4ab98f0a1f8613e9
milk953 <milk.the@hotmail.com>
2024-08-16 13:24:31 +0700

    new

--- routes/productMasterRoutes.cjs
+++ routes/productMasterRoutes.cjs
@@ -4,12 +4,8 @@ const productmaster = require("../WorkService/productmaster.cjs");
 
 
 
-router.post("/searchFactory", productmaster.searchFactory);
-router.post("/Factory", productmaster.getFactory);
-router.post("/SerialStructure", productmaster.getSerialStructure);
-router.post("/SheetStructure", productmaster.getSheetStructure);
-router.post("/SheetType", productmaster.getSheetType);
-router.post("/ProceesControl", productmaster.getProceesControl);
+router.post("/getdatatable", productmaster.getdatatable);
+router.post("/getupdatecount", productmaster.getupdatecount);
 router.post("/insProduct_Master", productmaster.insertProduct_Master);
 router.post("/updateProduct_Master", productmaster.updateProduct_Master);
 router.post("/delProduct_Master", productmaster.deleteProduct_Master);
