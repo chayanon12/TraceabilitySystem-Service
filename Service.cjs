548643a4bb92998132a922e46dc77744feee5460
milk953 <milk.the@hotmail.com>
2024-08-22 14:45:36 +0700

    addnewapi

--- Service.cjs
+++ Service.cjs
@@ -31,6 +31,7 @@ const ScanSMTSerialBackend = require("./routes/ScanSMTSerialBackendConfirmRoutes
 const AVIconfirm = require("./routes/AVIConfirm.cjs");
 const ScanAutoBendingTime = require("./routes/ScanAutoBendingTime.cjs");
 const ReJudgement = require("./routes/ReJudgement.cjs");
+const ScanSMTSerialPcs = require("./routes/ScanSMTSerialPcsAutoTrayRoutes.cjs");
 const cors = require('cors');
 app.use(cors({
   origin: '*', 
@@ -77,7 +78,7 @@ app.use("/api",ELT_Type)
 app.use("/api",AVIconfirm)
 app.use("/api", ScanAutoBendingTime);
 app.use("/api", ReJudgement);
-
+app.use("/api", ScanSMTSerialPcs);
 
 
 app.listen(port, () => {
---
+++ routes/ScanSMTSerialPcsAutoTrayRoutes.cjs
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const ScanSMTSerialPcs = require("../WorkService/Model_ScanSMTSerialPcsAutoTray.cjs");
+
+
+
+
+module.exports = router;
