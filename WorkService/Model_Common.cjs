adbad106b6b91ffca5570d1cb858dc1ab825be30
chayanon12 <chayanonolo@gmail.com>
2024-09-13 13:56:56 +0700

    pond

--- WorkService/Model_Common.cjs
+++ WorkService/Model_Common.cjs
@@ -1600,7 +1600,7 @@ module.exports.getProductDataFix = async function (req, res) {
     const client = await ConnectPG_DB();
     const result = await client.query(query);
     await DisconnectPG_DB(client);
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   } catch (err) {
     writeLogError(err.message, query);
     res.status(500).json({ message: err.message });
