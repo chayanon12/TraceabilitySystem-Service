0822642dc5987fd9837504a18a05e4fd1d80522e
khunpan9551 <paiboon.wongthongdee@gmail.com>
2024-09-21 11:22:00 +0700

    commit by khun

--- WorkService/Model_ScanAVIConfirmResult.cjs
+++ WorkService/Model_ScanAVIConfirmResult.cjs
@@ -8,13 +8,12 @@ const {
   const oracledb = require("oracledb");
   
   module.exports.GetProductDataAVIResultConfirm = async function (req, res) {
-    const { dataList } = req.body;
-    console.log("DD : ",dataList)
-    const json_convertdata = JSON.stringify(dataList);
+     const { strplantcode } = req.body;
+     console.log("DD : ",strplantcode)
     var query = "";
     try {
       const client = await ConnectPG_DB();
-      query += `SELECT "Traceability".trc_033_getproductdataaviresultconfirm('[${json_convertdata}]')`;
+      query += `SELECT * from "Traceability".trc_033_getproductdataaviresultconfirm('${strplantcode}')`;
       const result = await client.query(query);
       console.log("DATA SHOW trc_033_getproductdataaviresultconfirm : ",result.rows)
         res.status(200).json(result.rows);
@@ -24,3 +23,41 @@ const {
       res.status(500).json({ message: error.message });
     }
   };
+
+  module.exports.GetTestTypeAVIResultConfirm = async function (req, res) {
+    const { dataList } = req.body;
+    console.log("DD : ",dataList)
+    const json_convertdata = JSON.stringify(dataList);
+    var query = "";
+    try {
+      const client = await ConnectPG_DB();
+      query += `SELECT * from "Traceability".trc_033_gettesttypeaviresultconfirm('[${json_convertdata}]')`;
+      const result = await client.query(query);
+      console.log("DATA SHOW trc_033_gettesttypeaviresultconfirm : ",result.rows)
+        res.status(200).json(result.rows);
+        DisconnectPG_DB(client);
+    } catch (error) {
+      writeLogError(error.message, query);
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
+
+  module.exports.GetAVIResultConfirmDefault = async function (req, res) {
+    const { dataList } = req.body;
+    console.log("DD : ",dataList)
+    const json_convertdata = JSON.stringify(dataList);
+    var query = "";
+    try {
+      const client = await ConnectPG_DB();
+      query += `SELECT * from "Traceability".trc_033_getaviresultconfirmdefault('[${json_convertdata}]')`;
+      const result = await client.query(query);
+      console.log("DATA SHOW trc_033_getaviresultconfirmdefault : ",result.rows)
+        res.status(200).json(result.rows);
+        DisconnectPG_DB(client);
+    } catch (error) {
+      writeLogError(error.message, query);
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
