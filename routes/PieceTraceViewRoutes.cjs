b84580d463f3fe5d20401a0973565ce355c98ae8
milk953 <milk.the@hotmail.com>
2024-09-25 12:13:59 +0700

    ,

--- routes/PieceTraceViewRoutes.cjs
+++ routes/PieceTraceViewRoutes.cjs
@@ -2,4 +2,6 @@ const express = require("express");
 const router = express.Router();
 const PieceTraceView = require("../WorkService/Model_PieceTraceView.cjs");
 
+router.post("/GetSerialNo", PieceTraceView.GetSerialNo);
+
 module.exports = router;
