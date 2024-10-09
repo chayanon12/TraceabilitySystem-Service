d727480f0b0e91d005ab3c1c24ec412ee41e9758
khunpan9551 <paiboon.wongthongdee@gmail.com>
2024-10-09 10:21:10 +0700

    commit by khun 09/10/2024:10:21

--- WorkService/Model_SPIAOITimeView.cjs
+++ WorkService/Model_SPIAOITimeView.cjs
@@ -8,19 +8,17 @@ const {
   const oracledb = require("oracledb");
 
   module.exports.fnSheetSPIAOITimeData = async function (req, res) {
-    const { strlot } = req.body;
+    const { dataList } = req.body;
+    console.log("dataList:dataList", dataList)
+    const json_convertdata = JSON.stringify(dataList);
     var query = "";
-    let strResult = "NO"
     try {
       const client = await ConnectPG_DB();
-    //   query += `SELECT * FROM "Traceability".trc_039_getserialxrayresult('${strlot}')`;
+      query += ` SELECT * FROM "Traceability".trc_045_fnsheetspiaoitimedata('[${json_convertdata}]')`;
       const result = await client.query(query);
-      console.log("DATA SHOW trc_039_getserialxrayresult : ",result.rows)
-      if(result.rows.length > 0) {
-        strResult = result.rows[0].confirm_result  
-      }
-      console.log("DATA SHOW trc_039_getserialxrayresult : ",strResult)
-        res.status(200).json(strResult);
+      const filteredResult = result.rows.map(row => row.response);
+      console.log("DATA SHOW trc_045_fnsheetspiaoitimedata : ",filteredResult)
+      res.status(200).json(filteredResult);
         DisconnectPG_DB(client);
     } catch (error) {
       writeLogError(error.message, query);
