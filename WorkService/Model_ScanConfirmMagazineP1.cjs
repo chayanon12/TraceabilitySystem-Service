1c1514de8822c9d339a9e2d9be87f718350906b8
khunpan9551 <paiboon.wongthongdee@gmail.com>
2024-10-16 08:46:34 +0700

    commit by khun 16/10/2024 T:08:46

--- WorkService/Model_ScanConfirmMagazineP1.cjs
+++ WorkService/Model_ScanConfirmMagazineP1.cjs
@@ -12,12 +12,52 @@ module.exports.GetCountSerialByLotMagazine = async function (req, res) {
   try {
     const { dataList } = req.body;
     const json_convertdata = JSON.stringify(dataList);
+    console.log("GetCountSerialByLotMagazine",dataList);
     const client = await ConnectPG_DB();
     query += `select * from "Traceability".trc_000_common_GetCountSerialByLotMagazine('[${json_convertdata}]')`;
     const result = await client.query(query);
+    console.log(" DATA SHOW trc_000_common_GetCountSerialByLotMagazine",result.rows);
     res.status(200).json(result.rows);
   } catch (error) {
     writeLogError(error.message, query);
     res.status(500).json({ message: error.message });
   }
 };
+
+module.exports.SetManualConfirmMagazine = async function (req, res) {
+  let query = "";
+  try {
+    const { dataList } = req.body;
+    const json_convertdata = JSON.stringify(dataList);
+    console.log("SetManualConfirmMagazine",dataList);
+    const client = await ConnectPG_DB();
+    query += `CALL "Traceability".trc_055_setmanualconfirmmagazine('[${json_convertdata}]','')`;
+    const result = await client.query(query);
+    console.log(" DATA SHOW trc_055_setmanualconfirmmagazine",result.rows);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    writeLogError(error.message, query);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
+module.exports.GetSerialMagazineByLot = async function (req, res) {
+  const { dataList } = req.body;
+  const json_convertdata = JSON.stringify(dataList);
+  console.log("GetSerialMagazineByLot",dataList);
+  var query = "";
+  try {
+    const client = await ConnectPG_DB();
+    query += `CALL "Traceability".trc_055_getserialmagazinebylot('[${json_convertdata}]','','{}');`;
+    const result = await client.query(query);
+    console.log("DATA SHOW trc_055_getserialmagazinebylot : ",result.rows)
+      res.status(200).json(result.rows);
+      DisconnectPG_DB(client);
+  } catch (error) {
+    writeLogError(error.message, query);
+    res.status(500).json({ message: error.message });
+  }
+};
+
--- routes/ScanConfirmMagazineP1.cjs
+++ routes/ScanConfirmMagazineP1.cjs
@@ -3,6 +3,7 @@ const router = express.Router();
 const ScanConfirmMagazineP1 = require("../WorkService/Model_ScanConfirmMagazineP1.cjs");
 
 router.post("/GetCountSerialByLotMagazine", ScanConfirmMagazineP1.GetCountSerialByLotMagazine);
-
+router.post("/SetManualConfirmMagazine", ScanConfirmMagazineP1.SetManualConfirmMagazine);
+router.post("/GetSerialMagazineByLot", ScanConfirmMagazineP1.GetSerialMagazineByLot);
 
 module.exports = router;
