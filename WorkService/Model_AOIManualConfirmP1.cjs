93f1e90d8e6f12afea3d4b2c37a05fdb336d6301
khunpan9551 <paiboon.wongthongdee@gmail.com>
2024-10-17 19:51:14 +0700

    commit by khun 17/10/2024 T:19:51

--- WorkService/Model_AOIManualConfirmP1.cjs
+++ WorkService/Model_AOIManualConfirmP1.cjs
@@ -1,72 +1,45 @@
-// const {
-//     ConnectPG_DB,
-//     DisconnectPG_DB,
-//     ConnectOracleDB,
-//     DisconnectOracleDB,
-//   } = require("../Conncetion/DBConn.cjs");
-//   const { writeLogError } = require("../Common/LogFuction.cjs");
-//   const oracledb = require("oracledb");
+const {
+    ConnectPG_DB,
+    DisconnectPG_DB,
+    ConnectOracleDB,
+    DisconnectOracleDB,
+  } = require("../Conncetion/DBConn.cjs");
+  const { writeLogError } = require("../Common/LogFuction.cjs");
+  const oracledb = require("oracledb");
   
-//   module.exports.GetserialnoChangserial = async function (req, res) {
-//     const { dataList } = req.body;
-//     console.log("DD : ",dataList)
-//     const json_convertdata = JSON.stringify(dataList);
-//     var query = "";
-//     try {
-//       const client = await ConnectPG_DB();
-//       query += `SELECT "Traceability".trc_028_changserial_getserialno('[${json_convertdata}]')`;
-//       const result = await client.query(query);
-//       console.log("DATA SHOW trc_028_changserial_getserialno : ",result.rows)
-//         res.status(200).json(result.rows);
-//         DisconnectPG_DB(client);
-//     } catch (error) {
-//       writeLogError(error.message, query);
-//       res.status(500).json({ message: error.message });
-//     }
-//   };
+  module.exports.Update_aoiandspi_rslt = async function (req, res) {
+    const { dataList } = req.body;
+    console.log("Update_aoiandspi_rslt",dataList)
+    const json_convertdata = JSON.stringify(dataList);
+    var query = "";
+    try {
+      const client = await ConnectPG_DB();
+      query += `CALL "Traceability".trc_057_update_aoiandspi_rslt('[${json_convertdata}]','')`;
+      const result = await client.query(query);
+      console.log("DATA SHOW trc_057_update_aoiandspi_rslt : ",result.rows)
+        res.status(200).json(result.rows);
+        DisconnectPG_DB(client);
+    } catch (error) {
+      writeLogError(error.message, query);
+      res.status(500).json({ message: error.message });
+    }
+  };
   
-//   module.exports.GetserialnoChangserialoldnew = async function (req, res) {
-//     const { dataList } = req.body;
-//     console.log("GG : ",dataList)
-//     const json_convertdata = JSON.stringify(dataList);
-//     var query = "";
-//     try {
-//       const client = await ConnectPG_DB();
-  
-//       query += `CALL "Traceability".trc_028_changserial_getserialnooldnew('[${json_convertdata}]','','','{}');`;
-//       const result = await client.query(query);
-//       console.log("DATA SHOW trc_028_changserial_getserialnooldnew : ",result.rows)
-//         res.status(200).json(result.rows);
-//         DisconnectPG_DB(client);
-//     } catch (error) {
-//       console.log("DATA SHOW E : ")
-//       writeLogError(error.message, query);
-//       res.status(500).json({ message: error.message });
-//     }
-//   };
-  
-//   module.exports.SetserialnoChangserial = async function (req, res) {
-//     const { dataList } = req.body;
-//     const json_convertdata = JSON.stringify(dataList);
-//     console.log(dataList,"dataList")
-//     console.log(json_convertdata,"json_convertdata")
-//     var query = "";
-//     try {
-//       const client = await ConnectPG_DB();
-//       const { dataList } = req.body;
-//       const json_convertdata = JSON.stringify(dataList);
-//        query += `CALL "Traceability".trc_028_changserial_insertandupdateserialno('[${json_convertdata}]','')`;
-//       const result = await client.query(query);
-//       console.log("DATA SHOW trc_028_changserial_insertandupdateserialno : ",result.rows)
-//       if (result.rows.length > 0) {
-//         res.status(200).json(result.rows);
-//         DisconnectPG_DB(client);
-//       }
-//     } catch (error) {
-//       writeLogError(error.message, query);
-//       res.status(500).json({ message: error.message });
-//     }
-//   };
-  
-  
-  
+
+  module.exports.Search_aoiandspi_rslt = async function (req, res) {
+    let query = "";
+    try {
+      const { dataList } = req.body;
+      const json_convertdata = JSON.stringify(dataList);
+      console.log("Search_aoiandspi_rslt",dataList);
+      const client = await ConnectPG_DB();
+      query += `SELECT * FROM "Traceability".trc_057_search_aoiandspi_rslt('[${json_convertdata}]')`;
+      const result = await client.query(query);
+      const filteredResult = result.rows.map(row => row.response);
+      console.log("DATA SHOW trc_057_search_aoiandspi_rslt : ",filteredResult)
+      res.status(200).json(filteredResult);
+    } catch (error) {
+      writeLogError(error.message, query);
+      res.status(500).json({ message: error.message });
+    }
+  };
--- routes/AOIManualConfirmP1.cjs
+++ routes/AOIManualConfirmP1.cjs
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const AOIManualConfirmP1 = require("../WorkService/Model_AOIManualConfirmP1.cjs");
 
-// router.post("/GetCountSerialByLotMagazine", AOIManualConfirmP1.GetCountSerialByLotMagazine);
+router.post("/Update_aoiandspi_rslt", AOIManualConfirmP1.Update_aoiandspi_rslt);
+router.post("/Search_aoiandspi_rslt", AOIManualConfirmP1.Search_aoiandspi_rslt);
 
 module.exports = router;
