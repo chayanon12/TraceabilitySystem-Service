83eae13b73225ba8cbc6e6b3f6e7ef4ffd4f6a8d
milk953 <milk.the@hotmail.com>
2024-10-17 11:52:48 +0700

    finish

--- routes/ScanAOISheetNo.cjs
+++ routes/ScanAOISheetNo.cjs
@@ -3,5 +3,9 @@ const router = express.Router();
 const ScanAOISheetNo = require("../WorkService/Model_ScanAOISheetNo.cjs");
 
 router.post("/GetAOISheetCountbyLot", ScanAOISheetNo.GetAOISheetCountbyLot);
+router.post("/GetAOISheetDataByLot", ScanAOISheetNo.GetAOISheetDataByLot);
+router.post("/GetAOISheetCount", ScanAOISheetNo.GetAOISheetCount);
+router.post("/SetAOISheetNo", ScanAOISheetNo.SetAOISheetNo);
+router.post("/DeleteAOISheetNo", ScanAOISheetNo.DeleteAOISheetNo);
 
 module.exports = router;
