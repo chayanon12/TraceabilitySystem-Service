e25423d2e72eb6403f9c83f4f12bbee8ef3ba12c
milk953 <milk.the@hotmail.com>
2024-11-22 08:16:13 +0700

    milk

--- WorkService/Model_ScanSMTSerialControlTime.cjs
+++ WorkService/Model_ScanSMTSerialControlTime.cjs
@@ -26,7 +26,7 @@ module.exports.SetSerialProcControlTimeTable = async function (req, res) {
     var query = "";
     try {
         const p_data = JSON.stringify(req.body);
-        query = ` CALL "Traceability".trc_010_serialcontroltime_setserialproccontroltimetable('[${p_data}]', ''); `;
+        query = ` CALL "Traceability".trc_010_serialcontroltime_setserialproccontroltimetable('${p_data}', ''); `;
         const client = await ConnectPG_DB();
         const result = await client.query(query);
         await DisconnectPG_DB(client);
