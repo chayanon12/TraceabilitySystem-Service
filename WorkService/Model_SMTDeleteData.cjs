530342aacc5896ffe177233cb99cf0531230109d
milk953 <milk.the@hotmail.com>
2024-12-25 14:13:01 +0700

    milk

--- WorkService/Model_SMTDeleteData.cjs
+++ WorkService/Model_SMTDeleteData.cjs
@@ -6,6 +6,38 @@ const {
 } = require("../Conncetion/DBConn.cjs");
 const { writeLogError } = require("../Common/LogFuction.cjs");
 
+module.exports.GetSheetno = async function (req, res) {
+    var query = "";
+    try {
+        const p_data = JSON.stringify(req.body);
+        query = `SELECT * FROM "Traceability".trc_032_smtdeletedata_getsheetno('${p_data}'); `;
+
+        const client = await ConnectPG_DB();
+        const result = await client.query(query);
+        await DisconnectPG_DB(client);
+        res.status(200).json(result.rows);
+    } catch (err) {
+        writeLogError(err.message, query);
+        res.status(500).json({ message: err.message });
+    }
+};
+
+module.exports.GetRollLeaf = async function (req, res) {
+    var query = "";
+    try {
+        const p_data = JSON.stringify(req.body);
+        query = `SELECT * FROM "Traceability".trc_032_smtdeletedata_getrollleaf('${p_data}'); `;
+
+        const client = await ConnectPG_DB();
+        const result = await client.query(query);
+        await DisconnectPG_DB(client);
+        res.status(200).json(result.rows);
+    } catch (err) {
+        writeLogError(err.message, query);
+        res.status(500).json({ message: err.message });
+    }
+};
+
 module.exports.DeleteDataSheet = async function (req, res) {
     var query = "";
     try {
--- routes/SMTDeleteDataRoutes.cjs
+++ routes/SMTDeleteDataRoutes.cjs
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const SMTDeleteData = require("../WorkService/Model_SMTDeleteData.cjs");
 
+router.post("/getSheetno", SMTDeleteData.GetSheetno);
+router.post("/getRollLeaf", SMTDeleteData.GetRollLeaf);
 router.post("/DeleteDataSheet", SMTDeleteData.DeleteDataSheet);
 router.post("/DeleteDataRollleaf", SMTDeleteData.DeleteDataRollleaf);
 router.post("/getELTResult", SMTDeleteData.GetELTResult);
